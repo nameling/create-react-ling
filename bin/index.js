d6390c52b44aa735699782a75b447a6cd4042c77
xiaoling.wang <xiaoling.wang@weimob.com>
2019-11-13 17:58:54 +0800

    init: config

--- bin/index.js
+++ bin/index.js
@@ -3,3 +3,17 @@
 const commander = require("commander");
 const chalk = require("chalk");
 const pkg = require("../package.json");
+const config = require("../config");
+const { init, showInfo } = require("../init");
+
+commander
+.version(pkg.version)
+.option("<name>")
+.option("")
+
+commander.on("--help", showInfo);
+
+commander.parse(process.argv)
+
+console.log(`TYPE: ${chalk.bgBlue("React base")}`);
+init(config.branch.default, commander.args[0]);
--- init.js
+++ init.js
@@ -1,1 +1,79 @@
 const chalk = require("chalk");
+const ora  = require("ora");
+const fs = require("fs-extra");
+const shelljs = require("shelljs");
+const symbols = require('log-symbols');
+const git = require('isomorphic-git');
+const config = require("./config");
+
+
+git.plugins.set("fs", fs);
+
+let init = (branch, name) => {
+  const spinner = ora('正在初始化项目...');
+  const { gitUrl } = config;
+  if(!fs.existsSync(name)) {
+    console.log(`${chalk.red("Init project in:  ")}${chalk.green(name)}`);
+    spinner.start();
+    git.clone({
+      url: gitUrl,
+      singleBranch: true,
+      ref: branch,
+      depth: 1,
+      dir: name,
+      noTags: true
+    }).then(() => {
+      shelljs.rm("-rf", `${name}/.git`);
+      spinner.succeed();
+      console.log(symbols.success, chalk.green('Project initialized successfully'));
+      showExtra();
+    }).catch((e) => {
+      spinner.fail();
+      console.log(symbols.error, chalk.red(e));
+    })
+  } else {
+    console.log(symbols.error, chalk.red("该目录下已存在同名文件夹!"));
+  }
+}
+
+let showInfo = () => {
+  console.log("");
+  console.log("create app with react hooks");
+  console.log("");
+  console.log("Example:");
+  console.log("");
+  console.log(` ${chalk.bgBlue(" Basic: ")}`);
+  console.log(`     create-react-zlv ${chalk.green("<your_project_name_here>")}`);
+  console.log("");
+}
+
+let showExtra = () => {
+  console.log("");
+  console.log(`${chalk.cyan("npm run dev")}`);
+  console.log("   start the development server");
+  console.log("");
+  console.log(`${chalk.cyan("npm run build")}`);
+  console.log("   bundle your app for production");
+  console.log("");
+  console.log(`${chalk.cyan("npm run dll")}`);
+  console.log("   bundle dll.js and manifest.json");
+  console.log("   NOTICE: if you need dll, you should edit USE_DLL as true in webpack/config/config.js");
+  console.log("");
+  console.log(`${chalk.cyan("npm run analysis")}`);
+  console.log("   bundle your app for production and show you detail info about bundled files");
+  console.log("");
+  console.log(`${chalk.cyan("npm run lint-js")}`);
+  console.log("   format your js,jsx,ts,tsx file by eslint");
+  console.log("");
+  console.log(`${chalk.cyan("npm run lint-css")}`);
+  console.log("   format your .css, .less file by stylelint");
+  console.log("");
+  console.log(symbols.success, chalk.green('Project initialized successfully'));
+  console.log(`${chalk.cyan("enjoy coding!")}`);
+  console.log("");
+};
+
+module.exports = {
+  init,
+  showInfo
+};
