a2d9834ecc7a9b252617d990c15253cb784ed371
xiaoling.wang <xiaoling.wang@weimob.com>
2019-11-13 11:04:19 +0800

    init: precomit

---
+++ bin/index.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+const commander = require("commander");
+const chalk = require("chalk");
+const pkg = require("../package.json");
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports =  {
+    gitUrl: "https://github.com/nameling/react-starter-kit",
+    branch: {
+        default: "react-starter-kit"
+    },
+}
