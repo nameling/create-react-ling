553217d8f683206af1c5f14113743db34cb0cd79
xiaoling.wang <xiaoling.wang@weimob.com>
2019-11-15 14:18:45 +0800

    chore: remame

--- bin/index.js
+++ bin/index.js
@@ -2,18 +2,18 @@
 
 const commander = require("commander");
 const chalk = require("chalk");
-const pkg = require("../package.json");
+const pkg = require("../package");
 const config = require("../config");
 const { init, showInfo } = require("../init");
 
 commander
-.version(pkg.version)
-.option("<name>")
-.option("")
+    .version(pkg.version)
+    .option("<name>")
+    .option("");
 
 commander.on("--help", showInfo);
 
-commander.parse(process.argv)
+commander.parse(process.argv);
 
 console.log(`TYPE: ${chalk.bgBlue("React base")}`);
 init(config.branch.default, commander.args[0]);
